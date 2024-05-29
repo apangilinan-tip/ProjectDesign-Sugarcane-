fc576703bbf1fb7594ce39a996edb362f14e08d5
mrksngy_ <sangoyomark0@gmail.com>
2024-05-29 14:52:22 +0800

    camera preview reverted

--- PD_project/pages/dashboard.py
+++ PD_project/pages/dashboard.py
@@ -1,6 +1,6 @@
 from tkinter import *
-import cv2 
-from PIL import Image, ImageTk 
+import cv2
+from PIL import Image, ImageTk
 import os
 from datetime import datetime
 import threading
@@ -14,60 +14,64 @@ import queue
 class DashboardPage(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
-    
-        self.vid = cv2.VideoCapture(0) 
+
+        self.vid = cv2.VideoCapture(0)
         self.width, self.height = 320, 150
-        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, self.width) 
-        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height) 
+        self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 
         # Create a label for the camera preview
         self.label_widget = Label(self, borderwidth=5, relief="ridge")
-        self.label_widget.grid(row=1, column=0, padx=10, pady=10)  
+        self.label_widget.grid(row=1, column=0, padx=10, pady=10)
 
         self.capture_image = False
         self.image_count = 0
         self.capture_start_time = None
-        self.capture_end_time = None  # Initialize capture end time
+        self.capture_end_time = None
 
         # Create a label to display the image count
         self.count_label = Label(self, text="Images captured: 0")
-        self.count_label.grid(row=1, column=1, padx=10, pady=10)  
+        self.count_label.grid(row=1, column=1, padx=10, pady=10)
 
         # Create a button to capture an image
         self.capture_button = Button(self, text="Capture Image", command=self.toggle_capture)
-        self.capture_button.grid(row=3, column=0, padx=10, pady=10)  
+        self.capture_button.grid(row=3, column=0, padx=10, pady=10)
 
         # Create a button to stop capturing
         self.stop_button = Button(self, text="Stop Capture", command=self.stop_capture, state=DISABLED)
-        self.stop_button.grid(row=4, column=0, padx=10, pady=10)  
+        self.stop_button.grid(row=4, column=0, padx=10, pady=10)
 
-        self.bind('<Escape>', lambda e: self.master.destroy()) 
+        self.bind('<Escape>', lambda e: self.master.destroy())
 
         # Queue to pass captured images from camera thread to main GUI thread
         self.image_queue = queue.Queue()
 
-        self.session_detail_list = [] 
+        self.session_detail_list = []
         self.initialize_database()
 
-    def open_camera(self): 
+        # Start the camera preview thread
+        self.camera_thread = threading.Thread(target=self.open_camera)
+        self.camera_thread.daemon = True
+        self.camera_thread.start()
+        self.update_camera()  # Start updating the camera preview
+
+    def open_camera(self):
         while True:
             ret, frame = self.vid.read()  # Capture the video frame by frame
             if ret:  # Check if the frame is valid
-                opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
-                opencv_image = cv2.resize(opencv_image, (self.width, self.height))  
+                opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+                opencv_image = cv2.resize(opencv_image, (self.width, self.height))
                 captured_image = Image.fromarray(opencv_image)
                 # Put the captured image into the queue
                 self.image_queue.put(captured_image)
 
     def update_camera(self):
-        if self.capture_image and not self.image_queue.empty():
+        if not self.image_queue.empty():
             captured_image = self.image_queue.get()
             photo_image = ImageTk.PhotoImage(image=captured_image)
-            self.label_widget.photo_image = photo_image 
-            self.label_widget.configure(image=photo_image) 
+            self.label_widget.photo_image = photo_image
+            self.label_widget.configure(image=photo_image)
             self.label_widget.image = photo_image
-        elif not self.capture_image:
-            self.label_widget.configure(image=None)  # Clear the preview if not capturing
         self.label_widget.after(10, self.update_camera)  # Schedule the next update
 
     def refresh_app(self):
@@ -89,7 +93,6 @@ class DashboardPage(Frame):
 
     def toggle_capture(self):
         if not self.capture_image:
-            self.label_widget.grid(row=1, column=0, padx=10, pady=10) 
             self.capture_button.config(text="Capturing...")
             self.stop_button.config(state=NORMAL)
             self.capture_image = True
@@ -111,27 +114,18 @@ class DashboardPage(Frame):
             self.session_detail_list.clear()  # Clear session detail list
             self.capture_start_time = None  # Reset capture start time
             self.capture_end_time = None   # Reset end time
-            self.label_widget.configure(image=None)  # Clear the camera preview
-            self.label_widget.grid_forget()  # Hide the camera preview widget
             self.refresh_app()  # This should be called before releasing the camera
+
     def capture_images_continuously(self):
         if self.capture_image:
-            if not hasattr (self, "camera_thread"):
-                #Start the camera preview thread
-                self.camera_thread = threading.Thread(target=self.open_camera)
-                self.camera_thread.daemon = True
-                self.camera_thread.start()
-                self.update_camera()  # Start updating the camera preview
-                
             self.capture_image_func()  # Capture an image
             self.master.after(5000, self.capture_images_continuously)  # Capture image every 5 seconds
-            
 
     def capture_image_func(self):
         ret, frame = self.vid.read()
         if ret:
-            opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) 
-            captured_image = Image.fromarray(opencv_image) 
+            opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            captured_image = Image.fromarray(opencv_image)
             self.save_image(captured_image)
             self.image_count += 1
             self.count_label.config(text=f"Images captured: {self.image_count}")
@@ -149,14 +143,14 @@ class DashboardPage(Frame):
         with open(os.path.join(session_path, image_filename), "rb") as img_file:
             image_base64 = base64.b64encode(img_file.read()).decode("utf-8")
         
-        self.session_detail_list.append(image_filename)  #Save base64 or filename?
+        self.session_detail_list.append(image_filename)  # Save base64 or filename?
 
     def initialize_database(self):
         # self.client = MongoClient("mongodb://localhost:27017/")  # Connect to MongoDB
         self.client = MONGODB_URI  # Connect to MongoDB
         self.db = self.client["CaneCheck"]  # Select the database
         self.session_table = self.db["Session"]  # Select the collection
-        self.session_detail_table = self.db["SessionDetail"] 
+        self.session_detail_table = self.db["SessionDetail"]
 
     def ask_session_name(self):
         while True:
@@ -171,7 +165,6 @@ class DashboardPage(Frame):
                     return
             else:
                 return  # User canceled the operation, exit the loop without saving the session details
-
 
     def persist_to_database(self, session_name):
         if self.image_count > 0 and session_name:  # Increment session ID
@@ -206,12 +199,12 @@ class DashboardPage(Frame):
 
     def determine_variety_id(self, filename):  
         # Enter your code here to process the image on open cv
-        return "variety1" 
+        return "variety1"
 
     def exit_app(self):
         self.vid.release()  # Release the camera
         self.master.destroy()
-    
+
 if __name__ == "__main__":
     root = Tk()
     root.title("Dashboard Page")
